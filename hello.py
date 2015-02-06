c23d0605ea769acfb09104688a095ad11686de2f
Samuel Hodgkins <samuel.hodgkins@sky.com>
2015-02-06 21:47:45 +0000

    Switched frameworks to cyclone

--- hello.py
+++ hello.py
@@ -1,8 +1,27 @@
-import os
-from flask import Flask
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright 2010 Alexandre Fiori
+# based on the original Tornado by Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
-app = Flask(__name__)
+import sys
+from cyclone.bottle import run, route
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+
+@route("/")
+def index(web):
+    web.write("Hello, world")
+
+run(host="127.0.0.1", port=5000, log=sys.stdout)
